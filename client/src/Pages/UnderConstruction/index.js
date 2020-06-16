57a29068f0af84a05428e73a52503f291d313600
morcos daniel <mdanielibra@gmail.com>
2020-06-16 10:12:28 +0200

    client added

---
+++ client/src/Pages/UnderConstruction/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import InfoBar from '../../Components/InfoBar';
+import Title from '../../Components/Title';
+
+const UnderConstruction = ({ title, subTitle, infoTitle, info }) => (
+    <div>
+        <Title title={title} classes='mb-3 mt-3 text-center'
+            subTitle={subTitle} />
+        <InfoBar title={infoTitle} type="info" items={info} classes='mb-3' />
+
+    </div>
+)
+
+const mapStateToProps = state => ({
+    title: state.common.underConstruction.title,
+    subTitle: state.common.underConstruction.subTitle,
+    infoTitle: state.common.info.title,
+    info: state.common.underConstruction.info,
+})
+
+
+export default connect(mapStateToProps)(UnderConstruction)
