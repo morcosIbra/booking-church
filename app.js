a7fa6fb51d3656a68e72657e09dbd3a186b0a078
morcos daniel <mdanielibra@gmail.com>
2020-06-18 00:23:21 +0200

    7

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
@@ -32,7 +32,7 @@ app.get('/api/passwords', (req, res) => {
 
   console.log(`Sent ${count} passwords`);
 });
-app.get('/*', (req, res) => {
+app.get('/', (req, res) => {
   //  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   res.send('success booking church')
 });
